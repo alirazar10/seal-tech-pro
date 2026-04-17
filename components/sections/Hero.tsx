019232b279567072328059c8a3d27f9cfdcda607
Ali Reza <aliraza.r10@gmail.com>
2026-04-17 17:14:32 +0500

    fix: adjust bottom positioning of the wave in Hero component

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -130,7 +130,7 @@ export default function Hero() {
       </div>
 
       {/* Bottom wave */}
-      <div className="absolute bottom-0 left-0 right-0">
+      <div className="absolute -bottom-px left-0 right-0">
         <svg
           viewBox="0 0 1440 60"
           fill="none"
